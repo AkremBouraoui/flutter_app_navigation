62d2f75dca14c4b082d3c6fce055279b2b86985a
Akrem Bouraoui <bouraoui.akrem.25@hotmail.com>
2025-11-16 19:27:43 -0500

    étape 3 : Création de la page Infos

---
+++ lib/page_infos.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class PageInfos extends StatelessWidget {
+  const PageInfos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Page d'infos")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Bienvenue sur la page Infos !"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
